eb6f59aab4574d5510079dbbed48396fe15bc60c
zhouze <2909398929@qq.com>
2021-06-03 19:09:14 +0800

    忽略统一返回值注解

--- src/main/java/com/zhou/common/config/response/IgnoreResponseBody.java
+++ src/main/java/com/zhou/common/config/response/IgnoreResponseBody.java
@@ -1,9 +1,15 @@
 package com.zhou.common.config.response;
 
+import java.lang.annotation.*;
+
 /**
  * @author 周泽
  * @date Create in 11:06 2021/6/1
  * @Description 忽略统一返回值
  */
+@Documented
+@Inherited
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface IgnoreResponseBody {
 }
